bab16fd89ce00e68d65552642455ceca464d5bcd
Inna <inna.tereshchenko1@gmail.com>
2020-01-14 17:06:13 +0200

    Updates

---
+++ precache-manifest.a85e508dc803d6c204dd196fcd54ed04.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5159bdb2a5694ad0b4896d7e48c0884",
+    "url": "/vessel_page/index.html"
+  },
+  {
+    "revision": "16e294f10581a021b43d",
+    "url": "/vessel_page/static/js/main.f660448d.chunk.js"
+  },
+  {
+    "revision": "f31b837e90beeb7c3647",
+    "url": "/vessel_page/static/js/runtime~main.b005a8e1.js"
+  }
+]);
